6775a905f4d4ec0d5dbf9de01751b15fcac9dec8
BrotskyS <sergiypik.work@gmail.com>
2022-05-24 11:52:57 +0300

    Initial Commit

---
+++ Shared/MoneyVerterApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyVerterApp.swift
+//  Shared
+//
+//  Created by Sergiy Brotsky on 24.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct MoneyVerterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
